a200a752bba5950deb0ef2592eed2c50c50dc4ed
Riley King <rk584020@ohio.edu>
2022-01-19 17:29:42 -0500

    git-lab-program.cc

---
+++ git-lab-program.cc
@@ -0,0 +1,20 @@
+
+/*
+ *        File: git-lab-program.cc
+ *      Author: <Riley King>
+ *        Date: <1/19/22>
+ * Description: Git Lab
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    /*add code*/
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
